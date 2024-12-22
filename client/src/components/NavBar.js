b0390033f69b322bff3daf020835c1ef5eccb77c
parriagada <parriagadabarrera@gmail.com>
2024-12-22 18:38:40 -0300

    nav bar responsive

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './NavBar.css';
-import { Link, useLocation } from 'react-router-dom'; 
-import { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
-  const isLoggedIn = !!localStorage.getItem('token'); // Verifica si el token existe
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isLoggedIn = !!localStorage.getItem('token');
+  const location = useLocation();
 
   const handleLogout = () => {
-    localStorage.removeItem('token'); // Elimina el token del almacenamiento local
-    window.location.href = '/login'; // Redirige al usuario a la página de inicio de sesión
+    localStorage.removeItem('token');
+    window.location.href = '/login';
   };
 
-  const location = useLocation();
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   const generateTitle = () => {
     const path = location.pathname;
     switch (path) {
@@ -34,53 +38,52 @@ function Navbar() {
 
   useEffect(() => {
     document.title = generateTitle();
-  }, [location]); 
-
+  }, [location]);
 
   return (
     <nav className="navbar">
       <div className="nav-mobile">
         <div className="logo">HabitsUp</div>
-        <div className="menu-toggle">
-          <i className="fas fa-bars"></i>
-        </div>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          <i className={isMenuOpen ? "fas fa-times" : "fas fa-bars"}></i>
+        </button>
       </div>
-      <ul className="nav-list">
-        {/* Opciones para usuarios autenticados e invitados */}
+      <ul className={`nav-list ${isMenuOpen ? 'open' : ''}`}>
+        {/* Opciones para usuarios autenticados */}
         <li>
-          <Link to="/habitos" className={isLoggedIn ? '' : 'disabled'}>
+          <Link to="/habitos" className={isLoggedIn ? '' : 'disabled'} onClick={() => setIsMenuOpen(false)}>
             Hábitos
           </Link>
         </li>
         <li>
-          <Link to="/pomodoro" className={isLoggedIn ? '' : 'disabled'}>
+          <Link to="/pomodoro" className={isLoggedIn ? '' : 'disabled'} onClick={() => setIsMenuOpen(false)}>
             Pomodoro
           </Link>
         </li>
         <li>
-          <Link to="/comunidad" className={isLoggedIn ? '' : 'disabled'}>
+          <Link to="/comunidad" className={isLoggedIn ? '' : 'disabled'} onClick={() => setIsMenuOpen(false)}>
             Comunidad
           </Link>
         </li>
         <li>
-          <Link to="/perfil" className={isLoggedIn ? '' : 'disabled'}>
+          <Link to="/perfil" className={isLoggedIn ? '' : 'disabled'} onClick={() => setIsMenuOpen(false)}>
             Perfil
           </Link>
         </li>
 
         {isLoggedIn && (
           <li>
-            <Link to="/login" onClick={handleLogout}>Cerrar sesión</Link> 
+            <Link to="/login" onClick={() => { handleLogout(); setIsMenuOpen(false); }}>Cerrar sesión</Link>
           </li>
         )}
         {/* Opciones solo para usuarios no autenticados */}
         {!isLoggedIn && (
           <>
             <li>
-              <Link to="/registro">Registro</Link>
+              <Link to="/registro" onClick={() => setIsMenuOpen(false)}>Registro</Link>
             </li>
             <li>
-              <Link to="/login">Iniciar sesión</Link>
+              <Link to="/login" onClick={() => setIsMenuOpen(false)}>Iniciar sesión</Link>
             </li>
           </>
         )}
